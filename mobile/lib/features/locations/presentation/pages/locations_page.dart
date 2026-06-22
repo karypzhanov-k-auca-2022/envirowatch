d12fd835d1f5bc087d2cebc0864e2120e4f1436b
admintesting <kairat_k@auca.kg>
2026-06-22 21:39:34 +0600

    fix

--- mobile/lib/features/locations/presentation/pages/locations_page.dart
+++ mobile/lib/features/locations/presentation/pages/locations_page.dart
@@ -1,1 +1,12 @@
-class Lo
+import 'package:flutter/material.dart';
+
+class LocationsPage extends StatelessWidget {
+  const LocationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Locations Page')),
+    );
+  }
+}
--- mobile/lib/features/settings/presentation/pages/settings_page.dart
+++ mobile/lib/features/settings/presentation/pages/settings_page.dart
@@ -1,1 +1,12 @@
-class SettingsPage {}
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Settings Page')),
+    );
+  }
+}
